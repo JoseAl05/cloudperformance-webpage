319286232025ed122b946b54a2342d8eb8f99133
JPArancibia <jparancibia@intaclatam.com>
2025-04-02 15:07:27 -0300

    changed sidebar style

--- components/CollapseSidebar.tsx
+++ components/CollapseSidebar.tsx
@@ -7,6 +7,7 @@ import { usePathname } from "next/navigation"
 import { BarChart2, Box, Cloud, Home, PieChart, Zap, ChartLine, ChevronRight } from 'lucide-react';
 import { Button } from "@/components/ui/button"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import { LogoComponent } from './LogoComponent'
 
 export const CollapseSidebar = () => {
   const [isExpanded, setIsExpanded] = useState(false)
@@ -77,7 +78,7 @@ export const CollapseSidebar = () => {
       <div
         className={cn(
           "fixed left-0 top-0 h-full z-[101] flex flex-col transition-all duration-300 ease-in-out",
-          isExpanded ? "w-64 bg-gray-900" : "w-16 bg-[#1a1a1a]",
+          isExpanded ? "w-64 bg-white" : "w-16 bg-white",
         )}
       >
         <div className="flex justify-end p-2">
@@ -88,22 +89,25 @@ export const CollapseSidebar = () => {
 
         <div className="flex flex-col items-center gap-8 mt-4">
           <TooltipProvider delayDuration={300}>
+            <LogoComponent isExpanded={isExpanded}/>
             {routes.map((route) => (
               <div key={route.href} className="w-full flex justify-center">
                 {isExpanded ? (
-                  <Link
-                    href={route.href}
-                    className={cn(
-                      "flex items-center px-4 py-2 w-full text-gray-400 hover:text-white transition-colors",
-                      pathname === route.href && "text-white bg-gray-800",
-                      route.href === "#" && "justify-center",
-                    )}
-                  >
-                    <div className={cn("flex items-center justify-center w-10 h-10", )}>
-                      <route.icon className="h-6 w-6" />
-                    </div>
-                    {route.href !== "#" && <span className="ml-3 font-medium">{route.label}</span>}
-                  </Link>
+                  <>
+                    <Link
+                      href={route.href}
+                      className={cn(
+                        "flex items-center px-4 py-2 w-full text-slate-800 hover:text-slate-400 transition-colors",
+                        pathname === route.href && "text-slate-400 bg-gray-200",
+                        route.href === "#" && "justify-center",
+                      )}
+                    >
+                      <div className={cn("flex items-center justify-center w-10 h-10", )}>
+                        <route.icon className="h-6 w-6" />
+                      </div>
+                      <span className="ml-3 font-medium">{route.label}</span>
+                    </Link>
+                  </>
                 ) : (
                   <Tooltip>
                     <TooltipTrigger asChild>
--- components/LogoComponent.tsx
+++ components/LogoComponent.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -6,8 +7,8 @@ import { cn } from '@/lib/utils';
 
 const monsterrat = Montserrat({ weight: '600', subsets: ['latin'] });
 
-export const LogoComponent = () => {
-    return (
+export const LogoComponent = ({ isExpanded }: { isExpanded: boolean }) => {
+    return isExpanded ? (
         <Link href='/' className='flex items-center mb-0 md:pl-3 md:mb-14'>
             <div className='relative w-10 h-10 md:w-20 md:h-20 mr-2 mt-0 md:mt-3'>
                 <Image
@@ -19,5 +20,14 @@ export const LogoComponent = () => {
             </div>
             <h1 className={cn('text-base md:text-xl font-bold text-blue-600', monsterrat.className)}>Cloud Performance</h1>
         </Link>
-    )
+    ) : (
+        <div className='relative w-10 h-10 md:w-12 md:h-12 mr-2 mt-0 md:mt-3'>
+            <Image
+                width={100}
+                height={100}
+                alt='Logo Intac'
+                src='/logo.png'
+            />
+        </div>
+    );
 }
