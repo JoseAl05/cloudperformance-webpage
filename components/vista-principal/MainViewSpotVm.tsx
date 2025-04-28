496f2294f15cbda823a88818da4f882429e5dff9
felipenanculef <102937232+felipenanculef@users.noreply.github.com>
2025-04-28 13:32:09 -0400

    Update MainViewSpotVm.tsx

--- components/vista-principal/MainViewSpotVm.tsx
+++ components/vista-principal/MainViewSpotVm.tsx
@@ -55,7 +55,7 @@ export const MainViewSpotVm = () => {
                         </div>
                     ) : (
                         <div className='relative w-full rounded-lg overflow-hidden shadow-md border border-slate-200 transition-all duration-300'>
-                            <iframe title="Cloudperformance2.0" width="1280" height="520" src="https://app.powerbi.com/view?r=eyJrIjoiZTNjOGY4MjYtYjc1Ni00MDNkLTg1ZTMtY2ExMWIyNmU0NTMwIiwidCI6IjdiYjNlMTQ3LWQxZTgtNDQ4Yy05NGE0LTUyNjQyZGQ1ZGQ4ZCIsImMiOjR9&pageName=21bd632d4d2f7248f33b" className='w-full h-[50vh] sm:h-[60vh] md:h-[120vh]' style={{ clipPath: "inset(0px 0px 53px 0px)" }} frameBorder="0" allowFullScreen={true}></iframe>
+                            <iframe title="Cloudperformance2.0" width="1280" height="720" src="https://app.powerbi.com/view?r=eyJrIjoiZTNjOGY4MjYtYjc1Ni00MDNkLTg1ZTMtY2ExMWIyNmU0NTMwIiwidCI6IjdiYjNlMTQ3LWQxZTgtNDQ4Yy05NGE0LTUyNjQyZGQ1ZGQ4ZCIsImMiOjR9&pageName=21bd632d4d2f7248f33b" className='w-full h-[50vh] sm:h-[60vh] md:h-[120vh]' style={{ clipPath: "inset(0px 0px 53px 0px)" }} frameBorder="0" allowFullScreen={true}></iframe>
                         </div>
                     )}
                 </div>
