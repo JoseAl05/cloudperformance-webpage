20b9ad34c387dcb3c715b3e86420bb63d24bc8bc
felipenanculef <102937232+felipenanculef@users.noreply.github.com>
2025-04-29 10:07:21 -0400

    Update MainViewConsume.tsx

--- components/vista-consumo/MainViewConsume.tsx
+++ components/vista-consumo/MainViewConsume.tsx
@@ -48,7 +48,7 @@ export const MainViewConsume = () => {
             width='1280'
             height='720'
             src='https://app.powerbi.com/view?r=eyJrIjoiZTNjOGY4MjYtYjc1Ni00MDNkLTg1ZTMtY2ExMWIyNmU0NTMwIiwidCI6IjdiYjNlMTQ3LWQxZTgtNDQ4Yy05NGE0LTUyNjQyZGQ1ZGQ4ZCIsImMiOjR9&pageName=97ff25681034e4ee2078'
-            className='w-full h-[100vh] rounded-lg shadow-lg'
+            className='w-full h-[50vh] sm:h-[60vh] md:h-[120vh]'
             style={{ clipPath: 'inset(0px 0px 53px 0px)' }}
             frameBorder='0'
             allowFullScreen={true}
@@ -61,7 +61,7 @@ export const MainViewConsume = () => {
             width='1280'
             height='720'
             src='https://app.powerbi.com/view?r=eyJrIjoiZTNjOGY4MjYtYjc1Ni00MDNkLTg1ZTMtY2ExMWIyNmU0NTMwIiwidCI6IjdiYjNlMTQ3LWQxZTgtNDQ4Yy05NGE0LTUyNjQyZGQ1ZGQ4ZCIsImMiOjR9&pageName=8b5e438e3e249625e92c'
-            className='w-full h-[100vh] rounded-lg shadow-lg'
+            className='w-full h-[50vh] sm:h-[60vh] md:h-[120vh]'
             style={{ clipPath: 'inset(0px 0px 53px 0px)' }}
             frameBorder='0'
             allowFullScreen={true}
@@ -74,7 +74,7 @@ export const MainViewConsume = () => {
             width='1280'
             height='720'
             src='https://app.powerbi.com/view?r=eyJrIjoiZTNjOGY4MjYtYjc1Ni00MDNkLTg1ZTMtY2ExMWIyNmU0NTMwIiwidCI6IjdiYjNlMTQ3LWQxZTgtNDQ4Yy05NGE0LTUyNjQyZGQ1ZGQ4ZCIsImMiOjR9&pageName=fccf1ae01c449603b42c'
-            className='w-full h-[100vh] rounded-lg shadow-lg'
+            className='w-full h-[50vh] sm:h-[60vh] md:h-[120vh]'
             style={{ clipPath: 'inset(0px 0px 53px 0px)' }}
             frameBorder='0'
             allowFullScreen={true}
