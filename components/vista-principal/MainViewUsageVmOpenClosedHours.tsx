cea850eea0fca61f9704991272e0a199ffe363b7
felipenanculef <102937232+felipenanculef@users.noreply.github.com>
2025-05-02 11:38:26 -0400

    Update MainViewUsageVmOpenClosedHours.tsx

--- components/vista-principal/MainViewUsageVmOpenClosedHours.tsx
+++ components/vista-principal/MainViewUsageVmOpenClosedHours.tsx
@@ -61,7 +61,7 @@ export const MainViewUsageVmOpenClosedHours = () => {
                                 width='1280'
                                 height='720'
                                 src='https://app.powerbi.com/view?r=eyJrIjoiZTNjOGY4MjYtYjc1Ni00MDNkLTg1ZTMtY2ExMWIyNmU0NTMwIiwidCI6IjdiYjNlMTQ3LWQxZTgtNDQ4Yy05NGE0LTUyNjQyZGQ1ZGQ4ZCIsImMiOjR9&pageName=3bd4926a73ffc6a8ad81'
-                                className='w-full h-[50vh] sm:h-[60vh] md:h-[120vh]'
+                                className='w-full h-[50vh] sm:h-[60vh] md:h-[130vh]'
                                 style={{ clipPath: 'inset(0px 0px 53px 0px)' }}
                                 frameBorder='0'
                                 allowFullScreen={true}
