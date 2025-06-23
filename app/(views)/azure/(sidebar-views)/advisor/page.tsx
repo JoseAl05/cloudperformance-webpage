b581aba78a91bbae35495f18162566a003e374e7
JPArancibia <jparancibia@intaclatam.com>
2025-06-23 12:26:34 -0400

    added new view to azure dashboards

---
+++ app/(views)/azure/(sidebar-views)/advisor/page.tsx
@@ -0,0 +1,11 @@
+import { AdvisorViewComponent } from '@/components/azure/vista-advisor/AdvisorViewComponent';
+
+export default function MainPage() {
+    return (
+        <div className='mt-[70px] md:mt-[120px] lg:mt-[95px] flex flex-col gap-5'>
+            <div className='flex-1 flex flex-col gap-10 bg-gray-200 p-4 overflow-auto dark:bg-[#111827] md:px-16'>
+                <AdvisorViewComponent />
+            </div>
+        </div>
+    )
+}
