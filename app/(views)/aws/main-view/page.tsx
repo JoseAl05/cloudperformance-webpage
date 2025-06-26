c9b9ee69aeaec91a7bc282a4f0d69219f4f0f039
JPArancibia <jparancibia@intaclatam.com>
2025-06-26 17:17:03 -0400

    added new treemap for aws main view

--- app/(views)/aws/main-view/page.tsx
+++ app/(views)/aws/main-view/page.tsx
@@ -1,14 +1,10 @@
-// import { MainViewPayGTrend } from '@/components/azure/treemap/MainViewPayGTrend';
-// import { MainViewTreemap } from '@/components/azure/treemap/MainViewTreemap';
-import { Separator } from '@/components/ui/separator';
+import { TreemapViewServiceSelectionAws } from '@/components/aws/vista-treemap/TreemapViewServiceSelectionAws';
 
 export default function AhorroView (){
     return (
         <div className='mt-[70px] lg:mt-[95px] flex flex-col gap-5'>
-            <div className='flex-1 flex flex-col items-center gap-10 bg-gray-200 p-4 overflow-auto dark:bg-[#111827] md:px-16'>
-                {/* <MainViewTreemap/> */}
-                <Separator className='bg-slate-500 h-5'/>
-                {/* <MainViewPayGTrend /> */}
+            <div className='flex flex-col justify-start gap-2 bg-gray-200 p-4 overflow-auto dark:bg-[#111827] md:px-16'>
+                <TreemapViewServiceSelectionAws />
             </div>
         </div>
     )
