9ea3f45990103aee47288ca91ddba7f65f3558c1
JPArancibia <jparancibia@intaclatam.com>
2025-06-27 16:50:11 -0400

    removed treemap views temporarily

--- app/(views)/azure/main-view/page.tsx
+++ app/(views)/azure/main-view/page.tsx
@@ -6,8 +6,8 @@ export default function AhorroView (){
     return (
         <div className='mt-[70px] lg:mt-[95px] flex flex-col gap-5'>
             <div className='flex-1 flex flex-col items-center gap-10 bg-gray-200 p-4 overflow-auto dark:bg-[#111827] md:px-16'>
-                <MainViewTreemap/>
-                <Separator className='bg-slate-500 h-5'/>
+                {/* <MainViewTreemap/>
+                <Separator className='bg-slate-500 h-5'/> */}
                 <MainViewPayGTrend />
             </div>
         </div>
